52ed740fce1bcabbb1c49769e7ea1a3e099ff070
babak-majd <baabak.majd@gmail.com>
2024-05-31 16:59:17 +0330

    default layout get completed.

---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="flex flex-col lg:flex-col-reverse w-full !h-screen bg-base-100 bg-gradient-to-b from-[#E9F3F6] to-[#F1F6F7] p-2 gap-2 text-base-content font-iranyekan relative"
+    dir="rtl">
+    <div class="h-full rounded-md shadow-lg bg-white overflow-y-auto">
+      <NuxtPage />
+    </div>
+    <div class="h-16 rounded-md shadow-md bg-white flex flex-row items-center px-2">
+      <NuxtLink to="/profile" class="w-1/5 flex justify-around items-center" :class="active_section === 'profile' ? 'text-primary' : ''">پروفایل</NuxtLink>
+      <NuxtLink to="/reports" class="w-1/5 flex justify-around items-center" :class="active_section === 'reports' ? 'text-primary' : ''">گزارش‌ها</NuxtLink>
+      <div class="btn-square w-1/5 rounded-full flex justify-around items-center">
+        <NuxtLink to="/">
+          <SvgKonkourplus :width="40" />
+        </NuxtLink>
+      </div>
+      <NuxtLink to="/konkoleague" class="w-1/5 flex justify-around items-center" :class="active_section === 'konkoleague' ? 'text-primary' : ''">کنکولیگ</NuxtLink>
+      <NuxtLink to="/support" class="w-1/5 flex justify-around items-center" :class="active_section === 'support' ? 'text-primary' : ''">پشتیبانی</NuxtLink>
+    </div>
+  </div>  
+</template>
+
+<script setup>
+import ConfigStore from "../store/ConfigStore";
+
+const isPopupVisible = ref(false);
+const active_section = ref("");
+const route = useRoute();
+
+const reload_store = () => {
+  ConfigStore.reload()
+    .then(() => { })
+    .catch((error) => {
+      console.error("Error reloading store:", error);
+    });
+};
+
+onBeforeMount(() => {
+  active_section.value = route.path.split("/")[1].toLowerCase();
+  reload_store();
+});
+
+function change_active_section(section) {
+  active_section.value = section;
+}
+
+watch(() => route.path, () => {
+  change_active_section(route.path.split("/")[1]);
+});
+
+</script>
