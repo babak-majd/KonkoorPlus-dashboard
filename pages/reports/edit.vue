e377b7bd9f6c078f39411ce12ccc1a8185a38d26
babak-majd <baabak.majd@gmail.com>
2024-06-06 10:59:00 +0330

    creating report item handled.

--- pages/reports/edit.vue
+++ pages/reports/edit.vue
@@ -6,27 +6,39 @@
             <ToolsLoading class="w-32 h-32" />
         </div>
 
-        <div v-if="data?.length ?? 0 > 0" class="grid gap-2 lg:grid-cols-5">
+        <div class="grid gap-2 lg:grid-cols-5">
             <Accordion>
                 <template v-slot:label>
                     افزودن
                 </template>
-                <div class="relative">
-                    <div class="flex flex-row gap-2 pb-2">
-                        <div class="font-bold">درس:</div>
-                        
+                <div class="flex flex-col pb-2 gap-6">
+                    <div class="custom_input_box text-base-content w-full lg:w-full pt-4">
+                        <InputTextMarked dir="ltr" v-model="form.title" required id="input_title">
+                            <label class="cursor-text" for="input_title" dir="rtl">
+                                عنوان
+                            </label>
+                        </InputTextMarked>
                     </div>
-                    <div class="flex flex-row gap-2 pb-2">
-                        <div class="font-bold">مدت‌زمان:</div>
-                        
+                    <div class="custom_input_box text-base-content w-full lg:w-full">
+                        <InputTextMarked dir="ltr" v-model="form.duration" required id="input_duration" type="number">
+                            <label class="cursor-text" for="input_duration" dir="rtl">
+                                مدت زمان (به دقیقه)
+                            </label>
+                        </InputTextMarked>
                     </div>
-                    <div class="flex flex-row gap-2 pb-2">
-                        <div class="font-bold">توضیحات:</div>
-                        
+                    <div class="custom_input_box text-base-content w-full lg:w-full">
+                        <InputTextMarked dir="ltr" v-model="form.description" required id="input_description">
+                            <label class="cursor-text" for="input_description" dir="rtl">
+                                توضیحات
+                            </label>
+                        </InputTextMarked>
                     </div>
+                    <button @click="add_report_item()" class="custom_input_box btn-primary w-full lg:w-full">
+                        ذخیره
+                    </button>
                 </div>
             </Accordion>
-            <Accordion v-for="(item, index) in data">
+            <Accordion v-if="data?.length ?? 0 > 0" v-for="(item, index) in data">
                 <template v-slot:label>
                     {{ item.title }}
                 </template>
@@ -43,14 +55,12 @@
                         <div class="font-bold">توضیحات:</div>
                         <div>{{ item.description }}</div>
                     </div>
-                    <button @click="delete_report_item(item.uuid)" class="[&>svg]:fill-danger hover:bg-red-100 rounded-[3px] cursor-pointer absolute bottom-2 -left-2">
+                    <button @click="delete_report_item(item.uuid)"
+                        class="[&>svg]:fill-danger hover:bg-red-100 rounded-[3px] cursor-pointer absolute bottom-2 -left-2">
                         <SvgTrash />
                     </button>
                 </div>
             </Accordion>
-        </div>
-        <div v-else class="bg-secondary h-20 w-full flex justify-center items-center">
-            داده‌ای برای این گزارش وارد نشده‌است.
         </div>
     </div>
 </template>
@@ -59,34 +69,60 @@
 import Request from "~~/Api/Request";
 import jalaliMoment from "jalali-moment";
 
+const route = useRoute();
 const request = new Request;
 const data = ref({});
+const form = ref({
+    report: "",
+    lesson: "83f5a8f5-2c50-4b16-ac2c-b794bef7c5ba",
+    title: "",
+    description: "",
+    duration: "",
+});
 
 onMounted(() => {
     collect_report_items();
 });
 
 async function collect_report_items() {
-    await request
-        .get("reports/items")
-        .then((response) => {
-            data.value = response.data;
-            console.log(data.value);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    if (!route.query.ruid) {
+        navigateTo("/reports");
+    } else {
+        form.value.report = route.query.ruid
+        await request
+            .get("reports/items")
+            .then((response) => {
+                data.value = response.data;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 }
 
 async function delete_report_item(uuid) {
     await request
         .delete(`reports/items/${uuid}`)
         .then((response) => {
-            data.value = response.data;
-            if(response.ok){
-                navigateTo(".");
+            if (response.status_code === 204) {
+                collect_report_items()
             }
-            console.log(data.value);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+async function add_report_item() {
+    await request
+        .post(`reports/items`, form.value)
+        .then((response) => {
+            if (response.ok) {
+                collect_report_items()
+                form.value.title = ""
+                form.value.description = ""
+                form.value.duration = ""
+            }
         })
         .catch((err) => {
             console.log(err);
