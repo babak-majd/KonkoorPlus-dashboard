3587c00a19a8bfd5754d4972d317d1ba1d3d30cf
babak-majd <baabak.majd@gmail.com>
2024-06-11 20:26:54 +0330

    tools page get updated.

--- pages/tools/index.vue
+++ pages/tools/index.vue
@@ -1,3 +1,28 @@
 <template>
-    tools
+    <div class="grid lg:grid-cols-3 p-2 gap-2 w-full lg:w-fit">
+        <div
+            class="h-24 bg-gradient-to-r from-green-500 to-green-800 text-white flex flex-row justify-between items-center p-2 rounded-lg">
+            <div class="flex flex-col gap-4 w-full h-full justify-center">
+                <div class="flex flex-row justify-between">
+                    <div class="font-bold text-lg">ابزار محاسبه درصد</div>
+                    <div class="bg-green-400 rounded-badge px-2">بزودی...</div>
+                </div>
+                <div class="flex flex-row gap-2 text-sm justify-center">
+                    نرو ماشین حساب، اون اولین جایی نیست که میری :)
+                </div>
+            </div>
+        </div>
+        <div
+            class="h-24 bg-gradient-to-r from-yellow-500 to-yellow-800 text-white flex flex-row justify-between items-center p-2 rounded-lg">
+            <div class="flex flex-col gap-4 w-full h-full justify-center">
+                <div class="flex flex-row justify-between">
+                    <div class="font-bold text-lg">تقویم</div>
+                    <div class="bg-yellow-400 rounded-badge px-2">بزودی...</div>
+                </div>
+                <div class="flex flex-row gap-2 text-sm justify-center">
+                    واقعا می‌خوای تقویم شمسی نصب کنی؟ بیا بهت میگم.
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
