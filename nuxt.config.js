a333bb3ebdf5c1691502f5a011fff5fa6c00585f
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-09-13 23:32:29 +0330

    user plugin to store data in cookies configured

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-
+    '~/plugins/userCookie'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ plugins/userCookie.ts
@@ -0,0 +1,28 @@
+import type User from "~/interfaces/User"
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const userData = useCookie('user-data', { maxAge: 60 * 60 * 24 * 7 }) // This cookie is available for almost One week
+
+    function setUserData(data: any): void {
+        userData.value = data
+    }
+
+    function getUserData(): User {
+        return userData.value
+    }
+
+    function logout() {
+        userData.value = null
+    }
+
+    function isLogin() {
+        return userData.value !== null
+    }
+
+    nuxtApp.provide('userData', {
+        setUserData,
+        getUserData,
+        logout,
+        isLogin
+    })
+})
