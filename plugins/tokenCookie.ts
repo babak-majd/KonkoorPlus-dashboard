0502a2a1b68247ce4c3e87dd27b8e4969c70e987
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-09-14 13:39:00 +0330

    token cookie created as plugin

---
+++ plugins/tokenCookie.ts
@@ -0,0 +1,30 @@
+export default defineNuxtPlugin((nuxtApp) => {
+    const token = useCookie('token', { maxAge: 60 * 60 * 24 * 7 })
+
+    function getToken() {
+        return token.value
+    }
+
+    function setToken(data: string) {
+        token.value = data
+    }
+
+    function logout() {
+        token.value = null
+    }
+
+    function tokenIsSet() {
+        return token.value !== null
+    }
+
+    return {
+        provide: {
+            token: {
+                getToken,
+                setToken,
+                logout,
+                tokenIsSet,
+            }
+        }
+    }
+})
