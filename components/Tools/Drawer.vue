49fdacb26fa70983e110083733caa8543dc786ca
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-09-14 23:47:05 +0330

    Default layout mobile links move in drawer

---
+++ components/Tools/Drawer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="drawer">
+    <input :id="id" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-side">
+      <label :for="id" aria-label="close sidebar" class="drawer-overlay"></label>
+
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({ id: { type: String, required: true } })
+</script>
+
+<style></style>
