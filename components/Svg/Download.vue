ff887d9c8aa7efd852d9f32da443d591347a735c
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-09-17 19:11:21 +0330

    ToolsProgress component created

---
+++ components/Svg/Download.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-w-6">
+    <svg viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M8 15V1" stroke="#E52421" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M2 9L8 15L14 9" stroke="#E52421" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M15 19H1" stroke="#E52421" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  </div>
+</template>
