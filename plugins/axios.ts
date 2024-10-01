a1cbe01fa5314bcb6efc3bd9dd162c1b886fc439
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-10-02 01:09:27 +0330

    Update axios plugin to use pinia token store

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,23 +1,25 @@
 import axios from "axios"
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.hook('app:mounted', () => {
-        const token = useNuxtApp().$token
-        let bearerToken = null
+    const token = useToken()
+    let bearerToken = null
 
-        if (token.tokenIsSet()) {
-            bearerToken = `Token ${token.getToken()}`
+    if (token.tokenIsSet) {
+        bearerToken = `Token ${token.getToken()}`
+    }
+
+    const instance = axios.create({
+        baseURL: useRuntimeConfig().public.API_URL,
+        headers: {
+            Authorization: bearerToken,
+            Accept: 'application/json',
+            "Content-Type": "application/json"
         }
-
-        const instance = axios.create({
-            baseURL: useRuntimeConfig().public.API_URL,
-            headers: {
-                Authorization: bearerToken,
-                Accept: 'application/json',
-                "Content-Type": "application/json"
-            }
-        })
-
-        nuxtApp.provide('axios', instance)
     })
+
+    return {
+        provide: {
+            axios: instance
+        }
+    }
 })
