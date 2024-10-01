04e14448d24d442ec0090e8294ca49a1a7e3bd4e
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-10-01 23:58:05 +0330

    overview report page connect to API and fixed

--- pages/overview/report.vue
+++ pages/overview/report.vue
@@ -6,31 +6,30 @@
       <div class="print:hidden">
         <ToolsBreadcrumb :items="[{ text: 'نمای کلی', url: '/overview' }, 'گزارش']" />
       </div>
-      <div class="hidden print:block">{{ $userData.fullname() }}</div>
+      <div class="hidden print:block">{{ userData.fullname }}</div>
       <h3 class="text-2xl font-semibold">گزارش کلی</h3>
     </div>
     <!-- filter and report -->
     <div class="print:hidden flex flex-col gap-3 md:flex-row md:justify-between">
-      <select
+      <select @change="getCourseData()" v-model="query.lesson"
         class="bg-base-250 w-full md:max-w-64 p-2 rounded-lg focus-within:outline focus-within:outline-1 focus-within:border focus-within:outline-offset-4 focus-within:bg-white">
-        <option :value="null">انتخاب درس</option>
+        <option :value="null">انتخاب کنید</option>
         <option v-for="(lesson, index) in lessons" :key="index" :value="lesson.uuid">{{ lesson.text }}</option>
       </select>
-      <button onclick="print()"
+      <button onclick="print()" :class="query.lesson === null ? 'pointer-events-none opacity-35' : ''"
         class="flex items-center justify-center gap-2 border p-2 px-4 rounded-lg border-main text-main hover:bg-main-50 transition-all duration-200">
         <span>دریافت گزارش</span>
         <SvgDownload class="w-5" />
       </button>
     </div>
     <!-- chart -->
-    <div id="chartSection" class="flex w-full print:items-center print:justify-center">
-      <ToolsChartArea :categories="[
-        'فروردین', 'اردیبهشت', 'خرداد',
-        'تیر', 'مرداد', 'شهریور',
-        'مهر', 'آبان', 'آذر',
-        'دی', 'بهمن', 'اسفند']" :series="[
-          { data: [7, 18, 15, 23, 36, 28, 37, 25, 30, 35, 29, 36], name: 'میزان مطالعه', type: 'area' },
-        ]" />
+    <div id="chartSection" class="relative flex w-full print:items-center print:justify-center">
+      <ToolsChartArea :categories="data.map((item) => item.title)"
+        :series="[{ data: data.map((item) => item.value) }]" />
+      <div v-if="!(!!query.lesson)"
+        class="absolute top-0 left-0 w-full h-full bg-gray-50 z-50 flex flex-col items-center justify-center text-2xl xl:text-4xl">
+        لطفا درس را انتخاب کنید
+      </div>
     </div>
 
     <div class="flex flex-col gap-5 md:flex-row">
@@ -43,7 +42,7 @@
 
         <!-- progress bar -->
         <div class="flex flex-col gap-1 max-w-80 2xl:max-w-md">
-          <ToolsProgress :max="365" :value="68" />
+          <ToolsProgress :max="365" :value="passedDayOnAcademicYear()" />
           <div class="flex items-center justify-between text-sm p-2">
             <!-- progressed days -->
             <div class="flex flex-col gap-1">
@@ -51,7 +50,7 @@
                 <span class="text-base-500">روز های طی شده</span>
                 <span class="h-1 w-8 bg-main rounded-full"></span>
               </div>
-              <span class="font-semibold">68 روز</span>
+              <span class="font-semibold">{{ passedDayOnAcademicYear() }} روز</span>
             </div>
 
             <!-- not progressed days -->
@@ -60,7 +59,7 @@
                 <span class="text-base-500">روز های باقی مانده</span>
                 <span class="h-1 w-8 bg-main-200 rounded-full"></span>
               </div>
-              <span class="font-semibold">297 روز</span>
+              <span class="font-semibold">{{ 365 - passedDayOnAcademicYear() }} روز</span>
             </div>
           </div>
         </div>
@@ -81,7 +80,7 @@
               <div class="flex flex-col">
                 <h6>{{ item.name }}</h6>
                 <div class="flex items-center gap-2 text-base-500">
-                  <span>{{ item.test }} تست</span>
+                  <span>{{ item.tests }} تست</span>
                   <span>|</span>
                   <span>{{ item.hours }} ساعت</span>
                 </div>
@@ -108,16 +107,39 @@
 useHead({ title: 'گزارش کلی' })
 const loading = ref(false)
 const lessons = ref([])
-const data = ref([])
+
+const data = ref([
+  { title: '', value: 0 }
+])
 const topThird = ref([])
-const { $axios, $userData, $token } = useNuxtApp()
+const { $axios } = useNuxtApp()
+const remainDays = ref(-1)
+const query = ref({
+  lesson: null
+})
+
+const userData = useUserData()
+const token = useToken()
+const startDate = useStartDate().getStartDate()
 
 const topThirdColor = (index) => {
   return index === 0 ? 'bg-main' : (index === 1 ? 'bg-main-400' : 'bg-main-200')
 }
 
+const passedDayOnAcademicYear = () => {
+  if (remainDays.value === -1) {
+    let startOfYear = new Date(`${new Date().getFullYear()}-01-01`)
+    let differenceDays = new Date(startDate).getDate() - startOfYear.getDate()
+    let now = new Date()
+    now.setDate(now.getDate() + differenceDays)
+
+    remainDays.value = now.getDate() - startOfYear.getDate()
+  }
+
+  return remainDays.value
+}
+
 onMounted(() => {
-  getData()
   getLesson()
   getTopThird()
 })
@@ -130,13 +152,22 @@ function print() {
   window.print()
   document.body.innerHTML = originalContent
 }
+function getCourseData() {
+  if (query.value.lesson !== null) {
+    getData(query.value.lesson)
+  }
+}
 
-async function getData(uuid = null) {
+
+async function getData(uuid) {
   loading.value = true
   try {
-    let response = await $axios.get('statistics/lesson-overview')
+    let response = await $axios.get('statistics/lesson-overview', { params: { lesson: uuid } })
     if (response.data.ok) {
       data.value = response.data.data
+      if (data.value.length === 0) {
+        data.value = [{ title: 'بدون محتوا', value: 0 }]
+      }
     }
   } catch (exception) {
     console.log(exception)
@@ -146,14 +177,14 @@ async function getTopThird() {
   loading.value = true
   try {
     let response = await $axios.get('statistics/overview')
-    if (response.data.OK) {
+    if (response.data.ok) {
       let result = response.data.data.this_month
       topThird.value = result
       if (topThird.value.length === 0) {
         topThird.value = [
-          { hours: 1, test: 1, name: 'اطلاعات در دسترس نیست' },
-          { hours: 1, test: 1, name: 'اطلاعات در دسترس نیست' },
-          { hours: 1, test: 1, name: 'اطلاعات در دسترس نیست' },
+          { hours: 1, tests: 1, name: 'اطلاعات در دسترس نیست' },
+          { hours: 1, tests: 1, name: 'اطلاعات در دسترس نیست' },
+          { hours: 1, tests: 1, name: 'اطلاعات در دسترس نیست' },
         ]
       }
     }
@@ -163,18 +194,20 @@ async function getTopThird() {
     loading.value = false
   }
 }
-
 async function getLesson() {
   try {
-    let response = await $axios.get('fields')
+    let response = await $axios.get('fields/lessons')
     if (response.data.ok) {
       lessons.value = response.data.data
+      if (query.value.lesson === '') {
+        query.value.lesson = lessons.value[0].uuid
+      }
     }
   } catch (exception) {
     console.log(exception)
     if (exception.response.status === 401) {
-      $userData.logout()
-      $token.logout()
+      userData.logout()
+      token.logout()
       return navigateTo('/auth/login')
     }
   }
