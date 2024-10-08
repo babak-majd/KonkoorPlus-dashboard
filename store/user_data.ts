e533fffd2d00025f7ba1d503f33204f9d94b1b30
Babak Majd <baabak.majd@gmail.com>
2024-10-08 18:34:05 +0330

    user data get updated.

--- store/user_data.ts
+++ store/user_data.ts
@@ -1,36 +1,50 @@
-import { defineStore } from 'pinia'
-import type User from '~/interfaces/User'
+import { defineStore } from "pinia";
+import type User from "~/interfaces/User";
 
 export const useUserData = defineStore({
-  id: 'userData',
+  id: "userData",
   state: () => ({
-    userData: useCookie('user_data', { maxAge: 60 * 60 * 24 * 7 })
+    userData: useCookie("user_data", { maxAge: 60 * 60 * 24 * 7 }),
   }),
   actions: {
     setUserData(data: any) {
       if (typeof data !== "string") {
-        data = JSON.stringify(data)
+        data = JSON.stringify(data);
       }
-      this.userData = data
+      this.userData = data;
     },
     getUserData(): User {
-      let result = this.userData
+      let result = this.userData;
       if (result === "string") {
-        result = JSON.parse(result)
+        result = JSON.parse(result);
       }
-      return result
+      return result;
     },
     logout() {
-      this.userData = null
-    }
+      this.userData = null;
+    },
   },
   getters: {
-    isLogin: (state) => { return !!state.userData },
+    isLogin: (state) => {
+      return !!state.userData;
+    },
     fullname: (state) => {
       if (!!state.userData) {
-        return `${state.userData.first_name} ${state.userData.last_name}`
+        return `${state.userData.first_name} ${state.userData.last_name}`;
       }
-      return null
-    }
-  }
-})
+      return null;
+    },
+    grade: (state) => {
+      if (!!state.userData) {
+        return state.userData.grade;
+      }
+      return null;
+    },
+    field: (state) => {
+      if (!!state.userData) {
+        return state.userData.field.name;
+      }
+      return null;
+    },
+  },
+});
