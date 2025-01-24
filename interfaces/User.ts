b67ba1fe956008fe6a4fd050766a70a84460086a
Mohammad Mahdi <matio.nora2000@gmail.com>
2025-01-24 16:50:28 +0330

    User interface fields updated

--- interfaces/User.ts
+++ interfaces/User.ts
@@ -1,12 +1,13 @@
 export default interface User {
+  uuid: string|null,
   first_name: string;
   last_name: string,
-  grade: number,
-  has_advisor: boolean,
+  grade: number | null,
+  has_advisor: boolean|null,
   state: Uuid,
   phone_number: string,
   city: Uuid,
-  field: Uuid,
+  field: Uuid | null,
   gender: string,
   role: string
 }
