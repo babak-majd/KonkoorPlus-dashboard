8f2b6eb40fe07e3fe8d169e8cefe0a23c22c6618
Mohammad Mahdi <matio.nora2000@gmail.com>
2025-02-02 13:08:09 +0330

    runTimeConfig reconfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,9 +8,28 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  app: {
+    head: {
+      title: "konkourplus-dashboard",
+      htmlAttrs: {
+        lang: "en",
+      },
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
   css: [
     "@/assets/Fonts/YekanWebFonts/css/fontiran.css",
     "@/assets/css/main.css",
   ],
   modules: ['@pinia/nuxt'],
+
+  runtimeConfig: {
+    public: {
+      APP_NAME: process.env.APP_NAME,
+      DEV_ENV: Boolean(process.env.DEV_ENV === "true"),
+      APP_LOCALE: process.env.APP_LOCALE,
+      APP_FALLBACK_LOCALE: process.env.APP_FALLBACK_LOCALE,
+      API_URL: process.env.API_URL,
+    },
+  },
 })
